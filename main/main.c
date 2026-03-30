3f1e184f9631eac7dcbf3b57e0a333647fa97cca
Zheng0627 <2906640436@qq.com>
2026-03-30 15:50:51 +0800

    添加 oled.c 文件并更新 CMakeLists.txt 以包含新源文件

--- main/main.c
+++ main/main.c
@@ -1,15 +1,11 @@
+//ESPRESSIF official ESP-IDF includes
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-
+//coustomer includes
 
 void app_main(void)
 {
-    
-    while (1)
-    {
-        /* code */
-    }
 
 }
